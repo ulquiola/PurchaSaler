353197a445e9e85f32ccab85db84ee29b4199ba9
一心 <ulquiola@163.com>
2019-06-01 11:49:42 +0800

    修复商品信息显示不全的BUG

--- PurchaSalerUI/Controllers/HomeController.cs
+++ PurchaSalerUI/Controllers/HomeController.cs
@@ -1,27 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Models;
-using PurchaSalerUI.Models;
+using PurchaSalerUI.ViewModel;
 
 namespace PurchaSalerUI.Controllers
 {
     public class HomeController : Controller
     {
+        PurchaSalerEntities db = new PurchaSalerEntities();
+
         public ActionResult Index()
         {
-            PurchaSalerEntities db = new PurchaSalerEntities();
-            var goods = db.Goods.OrderBy(g => g.GoodID).Take(4);
-            var users = db.Users;
-            var shops = db.Shops;
-            IndexViewModel indexView = new IndexViewModel();
-            indexView.goods = goods;
-            indexView.users = users;
-            indexView.shops = shops;
-            return View(indexView);
+            //var data = db.Goods.OrderByDescending(g => g.GoodID).Take(20).ToList();
+            //return View(data);
+            var data = (from a in db.Goods
+                         join b in db.Shops on a.ShopID equals b.ShopID
+                         join c in db.Users on b.UserID equals c.UserID
+                         select new GoodViewModel()
+                         {
+                            GoodPhoto=a.GoodPhoto,
+                            GoodDescribe=a.GoodDescribe,
+                            Price=(float)a.Price,
+                            ShopName=b.ShopName,
+                            Avatar=c.Avatar,
+                            UserName=c.UserName
+                         }).ToList();
+            return View(data);
         }
+
+        //public ActionResult Index(string searchstring,int?page)
+        //{
+        //    var data = from g in db.Goods
+        //               select g;
+        //    if (!String.IsNullOrEmpty(searchstring))
+        //    {
+        //        data = data.Where(g => g.GoodTitle.Contains(searchstring));
+        //    }
+        //    else
+        //    {
+        //        page = 1;
+        //    }
+        //    int pageSize = 3;
+        //    int pageNumber = (page ?? 1);
+        //    return View(data.ToPagedList(pageNumber, pageSize));
+        //}
 
         public ActionResult About()
         {
---
+++ PurchaSalerUI/ViewModel/GoodViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace PurchaSalerUI.ViewModel
+{
+    public class GoodViewModel
+    {
+        public string GoodPhoto { get; set; }
+        public string GoodDescribe { get; set; }
+        public float Price { get; set; }
+        public string ShopName { get; set; }
+        public string Avatar { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
