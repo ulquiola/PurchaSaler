6a95cfc5aa6240a7c3d09cda3e7f785906d92097
一心 <ulquiola@163.com>
2019-06-08 16:24:23 +0800

    暂存

--- PurchaSalerUI/ViewModel/GoodInfo.cs
+++ PurchaSalerUI/ViewModel/GoodInfo.cs
@@ -6,13 +6,14 @@ using Models;
 
 namespace PurchaSalerUI.ViewModel
 {
-    public class GoodViewModel
+    public class GoodInfo
     {
         public string GoodTitle { get; set; }
         public string GoodPhoto { get; set; }
         public string GoodDescribe { get; set; }
         public int CategoryID { get; set; }
-        public float Price { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
         public string ShopName { get; set; }
         public string Avatar { get; set; }
         public string UserName { get; set; }
