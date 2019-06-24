9df062b6b257023e6f8a9509623724d2e7da3eb2
一心 <ulquiola@163.com>
2019-06-24 14:03:46 +0800

    有时间在更新 有些地方得重构

---
+++ DAL/SqlOrder.cs
@@ -0,0 +1,20 @@
+﻿using IDAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class SqlOrder : IOrder
+    {
+        PurchaSalerEntities db = new PurchaSalerEntities();
+
+        public void AddOrder()
+        {
+
+        }
+    }
+}
